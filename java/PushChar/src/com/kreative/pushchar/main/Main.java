d4ed3e7039fb5f659c88312d37896dda58220125
Rebecca G. Bettencourt <beckiejo@gmail.com>
2021-02-15 18:02:10 -0800

    Fix "illegal reflective access" warning

--- java/PushChar/src/com/kreative/pushchar/main/Main.java
+++ java/PushChar/src/com/kreative/pushchar/main/Main.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -89,6 +90,15 @@ public class Main {
 			try { System.setProperty("com.apple.mrj.application.apple.menu.about.name", "PushChar"); } catch (Exception e) {}
 			try { System.setProperty("apple.laf.useScreenMenuBar", "true"); } catch (Exception e) {}
 			try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); } catch (Exception e) {}
+			
+			try {
+				Method getModule = Class.class.getMethod("getModule");
+				Object javaDesktop = getModule.invoke(Toolkit.getDefaultToolkit().getClass());
+				Object allUnnamed = getModule.invoke(Main.class);
+				Class<?> module = Class.forName("java.lang.Module");
+				Method addOpens = module.getMethod("addOpens", String.class, module);
+				addOpens.invoke(javaDesktop, "sun.awt.X11", allUnnamed);
+			} catch (Exception e) {}
 			
 			try {
 				Toolkit tk = Toolkit.getDefaultToolkit();
